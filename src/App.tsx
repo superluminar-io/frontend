b0de0d684257778ca53181de1c371e3eceb255a7
VerenaTraub <verenatraubmail@gmail.com>
2023-04-25 18:29:32 +0200

    update styling and refa app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,17 @@
 import "./App.css";
-import { useState } from "react";
+import AddNotesForm from "./Components/AddNotesForm";
+
 import { NotesList } from "./Components/NotesList";
-import { createNote } from "./services/notes";
+
 
 function App() {
-  const [state, setState] = useState<any>({
-    title: "",
-    content: "",
-  });
-
-  const handleChange = (e: any) => {
-    setState({ ...state, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    console.log(state);
-    const data = { content: state.content, title: state.title };
-    await createNote(data);
-    setState({ title: "", content: "" });
-  };
-
   return (
-    <main>
-      <header>
-        <h1>My awesome notes app</h1>
+    <main style={{width: "100%"}}>
+      <header style={{width: "100%"}}>
+        <img src="logo.png" alt="logo" />
+        <h3>Notes app</h3>
       </header>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <label>What do you want to add to your list?</label>
-        <label>Title</label>
-        <input
-          name="title"
-          type="text"
-          value={state.title}
-          onChange={(e) => handleChange(e)}
-        />
-
-        <label>Content</label>
-        <input
-          name="content"
-          type="text"
-          value={state.content}
-          onChange={(e) => handleChange(e)}
-        />
-
-        <button>Add note</button>
-      </form>
+      <AddNotesForm />
       <NotesList />
     </main>
   );
--- src/services/notes.js
+++ src/services/notes.js
@@ -1,11 +1,11 @@
 import { api } from './api';
 
 export const listNotes = async () => {
-    const response = await api.get('/prod/notes');
+    const response = await api.get('/notes');
     return response.data;
 }
 
 export const createNote = async (data) => {
-    const response = await api.post('/prod/notes', data);
+    const response = await api.post('/notes', data);
     return response.data;
   };
